01c9e9f7b73e9a6ad2c063914ea5201d6df6c239
neg0uiu <willianx2002.ws@gmail.com>
2022-10-13 12:52:55 -0300

    adicionado "algarismo" para fazer a funcao aritmetica

--- urls.py
+++ urls.py
@@ -19,5 +19,5 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<idade>', views.hello)
+    path('<int:num1>/<int:num2>/<algarismo>/', views.hello)
 ]
